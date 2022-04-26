3d39985a4bdea8f5b3841e54bc90c99b4ba223e7
Lee <leepotter010@gmail.com>
2022-04-26 20:10:34 +1000

    Added height value to AVLNode and updated with insert recursion

--- src/com/company/AVLTree.java
+++ src/com/company/AVLTree.java
@@ -3,6 +3,7 @@ package com.company;
 public class AVLTree {
     private class AVLNode {
         private int value;
+        private int height;
         private AVLNode leftChild;
         private AVLNode rightChild;
 
@@ -26,6 +27,12 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
+        root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+
         return root;
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
     }
 }
