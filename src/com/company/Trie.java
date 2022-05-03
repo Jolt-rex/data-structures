da4f93ac1bd8078dae9fba45f8e4c7d84b6a5ca7
Lee <leepotter010@gmail.com>
2022-05-03 14:04:07 +1000

    Trie - changed array to hashmap for implementation detail

--- src/com/company/Trie.java
+++ src/com/company/Trie.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.HashMap;
+
 public class Trie {
     public static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -16,6 +18,18 @@ public class Trie {
         public String toString() {
             return "Value=" + this.value;
         }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
     }
 
     private Node root = new Node(' ');
@@ -23,10 +37,9 @@ public class Trie {
     public void insert(String word) {
         var current = root;
         for(var ch : word.toCharArray()) {
-            var index = ch - 'a';
-            if(current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if(!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
         }
         current.isEndOfWord = true;
     }
